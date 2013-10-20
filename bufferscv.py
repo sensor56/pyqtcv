b4743ee385e495d6a8d7c1d656ec38071fad1502
monclubelec <support@mon-club-elec.fr>
2013-10-20 16:41:48 +0200

    Update bufferscv.py

--- bufferscv.py
+++ bufferscv.py
@@ -13,6 +13,8 @@ RGB = None
 R, G, B = None, None, None
 Gray = None
 
+Memory=None
+
 Trans16S3C, Trans16S3C1, Trans16S3C2 = None, None, None
 Trans16S1C, Trans16S1C1, Trans16S1C2 = None, None, None
 Trans8U3C, Trans8U3C1, Trans8U3C2=None, None, None
@@ -20,3 +22,14 @@ Trans8U1C, Trans8U1C1, Trans8U1C2= None, None, None
 Trans32F3C=None
 
 # les buffers sont communs à tous les modules qui les utilisent 
+
+# autres objets communs utiles
+qImage=None
+qPixmap=None
+
+webcam=None 
+
+# gsvideo
+pipeline=None
+sink=None 
+
